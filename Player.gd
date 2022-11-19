c8400bcf4202603c90feb48b0e48f18dab59bb6a
jmulholland4014 <62855716+jmulholland4014@users.noreply.github.com>
2022-11-18 19:49:39 -0500

    removingUnnecesaryCode

--- Player.gd
+++ Player.gd
@@ -28,26 +28,7 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		vel.x += 1
 		facingDir = Vector2(1, 0)
-	
-	if Input.is_action_pressed("dash") and canDash:
-		print("Dash")
-		vel = facingDir.normalized() *999999999999
-		canDash = false
-		dashing = true
-		yield(get_tree().create_timer(2), "timeout")
-		dashing = false
-		canDash = true
 	vel = vel.normalized()
 
 	# move the player
 	move_and_slide(vel * moveSpeed)
-	for index in get_slide_count():
-		var collision = get_slide_collision(index)
-		print(collision)
-		if(collision.collider.name.begins_with("Platform")):
-			print("hello player")
-
-
-func _on_Platform_area_entered(area):
-	print("hello Player")
-	pass # Replace with function body.
