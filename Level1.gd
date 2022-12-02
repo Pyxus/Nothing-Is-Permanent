aad3330bef5aaae16e84bc723e20d804af32a387
jmulholland4014 <62855716+jmulholland4014@users.noreply.github.com>
2022-12-02 14:25:18 -0500

    BugFixed

--- Level1.gd
+++ Level1.gd
@@ -23,12 +23,15 @@ func player_touched_flower():
 		if not spawnX in array:
 			array.append(spawnX)
 		if array.size() >= areaGrid.get_grid_size().x:
-			kill_player()
+			break
 		spawnCell = areaGrid.get_cell(spawnX,0)
 		if(not areaGrid.is_spawn_cell(spawnCell)):
 			spawnCell = null
-	$Player.position = spawnCell.position + Vector2(32,32)
-	areaGrid.spawn_rose_bushes()
+	if(spawnCell == null):
+		kill_player()
+	else:
+		$Player.position = spawnCell.position + Vector2(32,32)
+		areaGrid.spawn_rose_bushes()
 
 func _on_AreaGrid_body_entered(body: PhysicsBody2D, coord: Vector2) -> void:
 	if body is Player:
